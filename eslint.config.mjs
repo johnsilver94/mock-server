72849e54beeaaaa2520a80825a6e4de7f347a313
Ion Cretu <cretuion94@gmail.com>
2024-11-21 20:30:10 +0200

    refactor: update Prettier and ESLint configurations; remove unused dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,11 @@
-import globals from "globals"
 import eslint from "@eslint/js"
-import tseslint from 'typescript-eslint';
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
+import globals from "globals"
+import tseslint from "typescript-eslint"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	{ files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
 	{ languageOptions: { globals: globals.node } },
 	eslint.configs.recommended,
-	...tseslint.configs.recommended,
-	eslintPluginPrettierRecommended
+	...tseslint.configs.recommended
 ]
